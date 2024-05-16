317b3d9c1056121eccbc6af0655d95d600a6c3a2
Varsto04 <ilya-ovchinnikov-04-04@yande.ru>
2024-05-17 01:46:02 +0300

    deletion of unused functions added to optimisation

--- three_address_code_optimisation.py
+++ three_address_code_optimisation.py
@@ -4,7 +4,10 @@ class Optimisation:
         self.function_call_tracking = function_call_tracking
 
     def __optimisation(self):
-        pass
+        for function in self.function_call_tracking:
+            if not self.function_call_tracking[function]:
+                del self.three_address_code[function]
 
     def start(self):
         self.__optimisation()
+        print(self.three_address_code)
