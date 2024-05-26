c09b54c6083d9128360bab67c808d7c42c61cc29
Varsto04 <ilya-ovchinnikov-04-04@yandex.ru>
2024-05-26 11:50:27 +0300

    added some constructs for translation to asm x86-64

--- assembler_code_generator.py
+++ assembler_code_generator.py
@@ -1,35 +1,125 @@
+import re
+from three_address_code_generation import ThreeAddressCodeInstruction
+import struct
+
+
 class AssemblerCodeGeneration:
     def __init__(self, three_address_code: dict, name_file: str, symbol_table: dict):
         self.three_address_code = three_address_code
         self.name_file = name_file
         self.symbol_table = symbol_table
+        self.storage_offsets_data_integer_type = {}
+        self.storage_offsets_data_real_type = {}
+        self.offset_counter_rbp = 0
+        self.LCPI_counter = 0
 
     def __generate(self):
+        # variables_data_section = []
         with open(f'{self.name_file}.asm', 'w') as file:
-            data_section = '.data\n'
-            const_section = '.const\n'
-            for name_table in self.symbol_table:
-                for name_ident in self.symbol_table[name_table].symbols:
-                    type_variable = self.symbol_table[name_table].symbols[name_ident][1]
-                    value_variable = self.symbol_table[name_table].symbols[name_ident][0]
-                    if type_variable != 'function' and value_variable == None:
-                        if type_variable == 'integer' or type_variable == 'real':
-                            data_section += f'\t{name_ident} dq 0\n'
-                        elif type_variable == 'boolean':
-                            data_section += f'\t{name_ident} db 0\n'
-                        elif type_variable == 'string':
-                            data_section += f'\t{name_ident} .asciz ""\n'
-                    elif value_variable != None:
-                        if type_variable == 'integer' or type_variable == 'real':
-                            const_section += f'\t{name_ident} dq {value_variable}\n'
-                        elif type_variable == 'boolean':
-                            const_section += f'\t{name_ident} db {value_variable}\n'
-                        elif type_variable == 'string':
-                            const_section += f'\t{name_ident} .asciz {value_variable}\n'
-            file.write(data_section)
-            file.write(const_section)
+            data_section = '\n\nsection .data'
+            file.write('section .text\n\tglobal _start')
 
-        # for key, value in three_address_code.items():
+            for key, value in self.three_address_code.items():
+                if key == 'main':
+                    file.write(f'\n\n_start:')
+                else:
+                    file.write(f'\n\n_{key}:')
+                file.write(f'\n\tpush rbp')
+                file.write(f'\n\tmov rbp, rsp')
+                self.offset_counter_rbp = 0
+                self.storage_offsets_data_integer_type = {}
+                self.offset_counter_rbp = 0
+                self.storage_offsets_data_real_type = {}
+                i = 0
+                for key2, value2 in self.three_address_code[key].items():
+                    if i != 0:
+                        file.write(f'\n_{key2}:')
+                    i += 1
+                    for value2_part in value2:
+                        if value2_part[0] == 'goto':
+                            file.write(f'\n\tjmp _{value2_part[1]}')
+                        if value2_part[1] == ':=' and re.compile(r't\d+$').match(value2_part[0]) and \
+                                value2_part[-1] == 'tmp':
+                            if isinstance(value2_part[2], ThreeAddressCodeInstruction):
+                                if value2_part[2].op == '+':
+                                    arg1_type = self.__define_data_type(value2_part[2].arg1)
+                                    arg2_type = self.__define_data_type(value2_part[2].arg2)
+                                    if arg1_type:
+                                        if arg1_type == 'integer':
+                                            file.write(f'\n\tmov eax, {value2_part[2].arg1}')
+                                        elif arg1_type == 'real':
+                                            data_section += f'\nLCPI0_{self.LCPI_counter}:'
+                                            binary = struct.pack('f', value2_part[2].arg1)
+                                            hex_value = hex(int.from_bytes(binary, byteorder='little'))
+                                            data_section += f'\n\tdq {hex_value}'
+                                            file.write(f'\n\tmovss xmm0, [LCPI0_{self.LCPI_counter}]')
+                                            self.LCPI_counter += 1
+                                        else:
+                                            pass
+                                    else:
+                                        arg1_type = self.__getting_data_about_variable(value2_part[2].arg1)
+                                        if arg1_type:
+                                            if arg1_type[1] == 'integer':
+                                                file.write(f'\n\tmov eax, dword [rbp - {self.storage_offsets_data_integer_type[value2_part[2].arg1]}]')
+                                            elif arg1_type[1] == 'real':
+                                                file.write(f'\n\tmovsd xmm0, qword [rip + {self.storage_offsets_data_real_type[value2_part[2].arg1]}]')
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    if arg2_type:
+                                        if arg2_type == 'integer':
+                                            file.write(f'\n\tadd eax, {value2_part[2].arg2}')
+                                        else:
+                                            pass
+                                    else:
+                                        arg2_type = self.__getting_data_about_variable(value2_part[2].arg2)
+                                        if arg2_type:
+                                            if arg2_type[1] == 'integer':
+                                                file.write(f'\n\tadd eax, dword [rbp - {self.storage_offsets_data_integer_type[value2_part[2].arg2]}]')
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                        elif value2_part[1] == ':=':
+                            variable_type = self.__getting_data_about_variable(value2_part[0])
+                            if variable_type:
+                                if variable_type[1] == 'integer':
+                                    variable_value_data_type = self.__define_data_type(value2_part[2])
+                                    if variable_value_data_type:
+                                        file.write(f'\n\tmov dword [rbp - {self.offset_counter_rbp}], {value2_part[2]}')
+                                    else:
+                                        file.write(f'\n\tmov dword [rbp - {self.offset_counter_rbp}], eax')
+                                    self.storage_offsets_data_integer_type[value2_part[0]] = self.offset_counter_rbp
+                                    self.offset_counter_rbp += 4
+                                if variable_type[1] == 'real':
+                                    variable_value_data_type = self.__define_data_type(value2_part[2])
+                                    if variable_value_data_type:
+                                        file.write(f'\n\tmovss dword [rbp - {self.offset_counter_rbp}], {value2_part[2]}')
+                                    else:
+                                        file.write(f'\n\tmovss dword [rbp - {self.offset_counter_rbp}], xmm0')
+                                    self.storage_offsets_data_real_type[value2_part[0]] = self.offset_counter_rbp
+                                    self.offset_counter_rbp += 4
+                                else:
+                                    pass
+            file.write(data_section)
+
+    def __define_data_type(self, data):
+        if isinstance(data, float):
+            return "real"
+        if isinstance(data, int):
+            return "integer"
+        if str(data) == 'True' or str(data) == 'False':
+            return 'boolean'
+        if re.compile(r'^"(.*)"$').match(data):
+            return 'string'
+        return False
+
+    def __getting_data_about_variable(self, name_variable):
+        for name_table in self.symbol_table:
+            for name_ident in self.symbol_table[name_table].symbols:
+                if name_ident == name_variable:
+                    return self.symbol_table[name_table].symbols[name_ident]
 
     def start(self):
         self.__generate()
